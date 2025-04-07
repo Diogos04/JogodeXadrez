dca92ac8e162c628dc5021f6573a09c1b04c326a
Diogos04 <dioguinhotugazancan@gmail.com>
2025-04-06 21:03:17 -0300

    Att2

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
     cells.forEach(cell => {
         cell.addEventListener("click", () => {
             if (selectedPiece) {
-                selectedCell.classList.remove("selected"); // Remove o destaque da célula anterior
-                cell.innerHTML = selectedPiece; // Move a peça para a nova célula
+                selectedCell.classList.remove("selected");
+                cell.innerHTML = selectedPiece;
                 selectedPiece = null;
                 selectedCell = null;
             } else if (cell.innerHTML.trim() !== "") {
                 selectedPiece = cell.innerHTML;
                 selectedCell = cell;
                 cell.innerHTML = "";
-                cell.classList.add("selected"); // Destaca a célula selecionada
+                cell.classList.add("selected");
             }
         });
     });
